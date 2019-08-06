d6c6ac37d7341f4b73c6244832750af6675e14f9
Mehrdad Pazooki <pazookime@gmail.com>
2019-08-06 18:58:44 -0400

    sensors service

--- api/sensors/sensors_service.py
+++ api/sensors/sensors_service.py
@@ -1,24 +1,37 @@
 import trio
 from api.sensors.sensor import BaseSensor, CameraSensor
+import redis
+import time
+import json
 
 
 class SensorService:
     def __init__(self):
         self.sensors = []
+        self.redis = redis.Redis(host='localhost', port=6379, db=0)
 
     def register(self, sensor):
         self.sensors.append(sensor)
 
-    async def publish(self, channel, value):
-        print(channel, value)
+    async def subscribe(self, channel, callable):
+        return await self.redis.subscribe(channel, callable)
+
+    async def publish(self, channel, value, timestamp_ms):
+        message = json.dump({
+            'timestamp': timestamp_ms,
+            'value': value
+        })
+        print(channel, message.get('timestamp'), len(message.get('value')))
+        self.redis.publish(channel, message)
 
     async def run(self):
         while True:
             async with trio.open_nursery() as nursery:
+                timestamp_ms = int(time.time() * 1000)
                 for sensor in self.sensors:
                     nursery.start_soon(sensor.start)
                     nursery.start_soon(sensor.read)
-                    await self.publish(sensor.name, sensor.result)
+                    await self.publish(sensor.name, sensor.result, timestamp_ms)
 
 
 if __name__ == '__main__':
