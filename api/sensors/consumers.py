da7a72fdf515557ca5dde0529aa6419db12609c2
Mehrdad Pazooki <pazookime@gmail.com>
2019-08-07 19:00:40 -0400

    sensors service

--- api/sensors/consumers.py
+++ api/sensors/consumers.py
@@ -7,7 +7,7 @@ ss = SensorService()
 for msg in ss.subscribe('CameraSensor').listen():
     if msg['type'] == 'subscribe':
         if msg['data'] == 1:
-            log.info('subscribed to: %s' % (msg['channel']))
+            print('subscribed to: %s' % (msg['channel']))
     elif msg['type'] == 'message':
         value = json.loads(msg.get('data'))
         raw_img = base64.b64decode(value.get('value'))
