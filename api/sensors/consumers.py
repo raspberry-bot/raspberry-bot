22ad321e71ddd2e72eff44031f5f66483f577995
Mehrdad Pazooki <pazookime@gmail.com>
2019-08-07 18:59:40 -0400

    sensors service

--- api/sensors/consumers.py
+++ api/sensors/consumers.py
@@ -1,13 +1,17 @@
-import trio
-import redis
+from sensors_service import SensorService
+
 import json
 import base64
-from sensors_service import SensorService
 
 ss = SensorService()
 for msg in ss.subscribe('CameraSensor').listen():
-    if msg.get('type') == 'message':
+    if msg['type'] == 'subscribe':
+        if msg['data'] == 1:
+            log.info('subscribed to: %s' % (msg['channel']))
+    elif msg['type'] == 'message':
         value = json.loads(msg.get('data'))
         raw_img = base64.b64decode(value.get('value'))
         print(value.get('ts'))
         print(raw_img)
+    else:
+        print m
