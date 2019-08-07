2f5217ed968cd0cd26476ce781f824d203c01734
Mehrdad Pazooki <pazookime@gmail.com>
2019-08-07 18:50:46 -0400

    sensors service

--- api/sensors/consumers.py
+++ api/sensors/consumers.py
@@ -7,7 +7,7 @@ from sensors_service import SensorService
 ss = SensorService()
 for msg in ss.subscribe('CameraSensor'):
     import pdb;pdb.set_trace()
-    value = json.loads(msg.get('value'))
+    value = json.loads(msg.get('data'))
     raw_img = base64.decode(value)
     print(value.get('ts'))
     print(raw_img)
