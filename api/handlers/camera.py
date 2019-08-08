bd8b559a994e3cb8c4073c56befdf9deb45089c8
Mehrdad Pazooki <pazookime@gmail.com>
2019-08-07 20:07:56 -0400

    sensors service

--- api/handlers/camera.py
+++ api/handlers/camera.py
@@ -29,7 +29,7 @@ class CameraHandler(tornado.websocket.WebSocketHandler):
         msg = self.camera_channel.get_message()
         if msg and msg.get('type') in ['message']:
             value = json.loads(msg.get('data'))
-            raw_img = value.get('value')
+            raw_img = value.get('value').encode()
             return raw_img
 
     def on_close(self):
--- api/sensors/sensor.py
+++ api/sensors/sensor.py
@@ -53,5 +53,5 @@ class CameraSensor(BaseSensor):
             pimg = Image.frombytes("RGB", img.get_size(), imgstr)
             with io.BytesIO() as bytesIO:
                 pimg.save(bytesIO, "JPEG", quality=self.quality, optimize=True)
-                self.result = base64.b64encode(bytes(bytesIO.getvalue(), "utf-8"))
+                self.result = base64.b64encode(bytesIO.getvalue()).decode()
                 return self.result
