32ef874f88b078ba351813b28175582ea8bb18d3
Mehrdad Pazooki <pazookime@gmail.com>
2019-08-22 18:08:07 -0400

    buttons event handler fixed for mobile

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -42,28 +42,29 @@ function sendControlData(controlKey) {
 
 var fullscreenElement = document.getElementById("main");
 function toggleFullscreen() {
-    if (!document.fullscreenElement && !document.mozFullScreenElement &&
-        !document.webkitFullscreenElement && !document.msFullscreenElement) {
-        if (fullscreenElement.requestFullscreen) {
-            fullscreenElement.requestFullscreen();
-        } else if (fullscreenElement.msRequestFullscreen) {
-            fullscreenElement.msRequestFullscreen();
-        } else if (fullscreenElement.mozRequestFullScreen) {
-            fullscreenElement.mozRequestFullScreen();
-        } else if (fullscreenElement.webkitRequestFullscreen) {
-            fullscreenElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-        }
-    } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        }
-    }
+    alert('Test')
+    // if (!document.fullscreenElement && !document.mozFullScreenElement &&
+    //     !document.webkitFullscreenElement && !document.msFullscreenElement) {
+    //     if (fullscreenElement.requestFullscreen) {
+    //         fullscreenElement.requestFullscreen();
+    //     } else if (fullscreenElement.msRequestFullscreen) {
+    //         fullscreenElement.msRequestFullscreen();
+    //     } else if (fullscreenElement.mozRequestFullScreen) {
+    //         fullscreenElement.mozRequestFullScreen();
+    //     } else if (fullscreenElement.webkitRequestFullscreen) {
+    //         fullscreenElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    //     }
+    // } else {
+    //     if (document.exitFullscreen) {
+    //         document.exitFullscreen();
+    //     } else if (document.msExitFullscreen) {
+    //         document.msExitFullscreen();
+    //     } else if (document.mozCancelFullScreen) {
+    //         document.mozCancelFullScreen();
+    //     } else if (document.webkitExitFullscreen) {
+    //         document.webkitExitFullscreen();
+    //     }
+    // }
 }
 
 function setupCameraDriveWebSockets() {
