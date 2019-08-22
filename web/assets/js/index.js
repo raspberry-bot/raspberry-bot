7bcb521922031ca1db65311510f069a93ee187c7
Mehrdad Pazooki <pazookime@gmail.com>
2019-08-22 16:51:30 -0400

    buttons event handler fixed for mobile

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -204,7 +204,10 @@ $(document).ready(function () {
     window.addEventListener('resize', resizeCanvas, false);
     resizeCanvas();    /// call the first time page is loaded
 
-    document.getElementById('fullscreenButton').addEventListener('click', function () {
+    document.getElementById('fullscreenButton').addEventListener('click touchstart', function () {
         toggleFullscreen();
+    });
+    document.getElementById('stopMotorsButton').addEventListener('click touchstart', function () {
+        stopMotors();
     });
 });
