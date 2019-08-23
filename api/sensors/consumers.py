3b2f71c10bb0364385cf2a3d20d36d8b2a6d99cf
Mehrdad Pazooki <pazookime@gmail.com>
2019-08-23 17:51:46 -0400

    data collection

--- api/sensors/consumers.py
+++ api/sensors/consumers.py
@@ -4,14 +4,18 @@ import json
 import base64
 
 ss = SensorService()
-for msg in ss.subscribe(['CameraSensorBackend', 'Drive']).listen():
+drive_f = open('../../data/drive_commands.log', 'a+')
+for msg in ss.subscribe(['CameraSensorData', 'Drive']).listen():
     if msg['type'] == 'subscribe':
         if msg['data'] == 1:
             print('subscribed to: %s' % (msg['channel']))
     elif msg['type'] == 'message':
-        if msg.get('data').get('channel') in ['CameraSensorBackend']:
-            value = json.loads(msg.get('data'))
+        value = json.loads(msg.get('data'))
+        if msg.get('data').get('channel') in ['CameraSensorData']:
             raw_img = base64.b64decode(value.get('value'))
-        
+            with open('../../data/images/' + str(value.get('ts')) + '.jpg', 'w+') as img_f:
+                img_f.write(raw_img)
+        elif msg.get('data').get('channel') in ['Drive']:
+            drive_f.write(str(value) + '\n')
     else:
         print(msg)
--- api/sensors/sensors_service.py
+++ api/sensors/sensors_service.py
@@ -6,9 +6,9 @@ import json
 
 
 class SensorService:
-    def __init__(self):
+    def __init__(self, host='localhost'):
         self.sensors = []
-        self.redis = redis.Redis(host='localhost', port=6379, db=0)
+        self.redis = redis.Redis(host=host, port=6379, db=0)
 
     def register(self, sensor):
         self.sensors.append(sensor)
@@ -24,7 +24,6 @@ class SensorService:
             'value': value,
             'channel': channel
         }
-        # print(('sending data to ', channel, timestamp_ms))
         self.redis.publish(channel, json.dumps(message))
 
     async def run(self):
@@ -34,12 +33,10 @@ class SensorService:
                     sensor.publish = self.publish
                     nursery.start_soon(sensor.start)
                     nursery.start_soon(sensor.read)
-                    # await self.publish(sensor.name, sensor.result)
 
 
 if __name__ == '__main__':
     ss = SensorService()
     ss.register(BaseSensor())
     ss.register(CameraSensor(width=640, height=480, quality=80))
-    # ss.register(CameraSensorBackend(width=640, height=480, quality=80))
     trio.run(ss.run)
