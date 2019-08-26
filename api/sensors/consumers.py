8801fbbbdb2500e6313a0ceb9db909a266648c81
Mehrdad Pazooki <pazookime@gmail.com>
2019-08-26 12:35:53 -0400

    data collection

--- api/sensors/consumers.py
+++ api/sensors/consumers.py
@@ -6,15 +6,15 @@ import base64
 ss = SensorService()
 drive_f = open('/opt/raspberry-bot/src/data/drive_commands.log', 'w+')
 for msg in ss.subscribe(['CameraSensorData', 'Drive']).listen():
-    print(json.loads(msg.get('data').decode("utf-8")))
     if msg['type'] == 'subscribe':
         if msg['data'] == 1:
             print('subscribed to: %s' % (msg['channel']))
     elif msg['type'] == 'message':
-        value = json.loads(msg.get('data'))
+        value = json.loads(msg.get('data').decode("utf-8"))
         if msg.get('data').get('channel') in ['CameraSensorData']:
             raw_img = base64.b64decode(value.get('value'))
-            with open('/opt/raspberry-bot/src/data/images/' + str(value.get('ts')) + '.jpg', 'w+') as img_f:
+            image_file_name = '/opt/raspberry-bot/src/data/images/' + str(value.get('ts')) + '.jpg'
+            with open(image_file_name, 'w+') as img_f:
                 img_f.write(raw_img)
         elif msg.get('data').get('channel') in ['Drive']:
             drive_f.write(str(value) + '\n')
