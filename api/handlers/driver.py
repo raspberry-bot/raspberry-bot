27f3f53b716163e6b58512685c348d63f466cb7d
Mehrdad Pazooki <pazookime@gmail.com>
2019-08-26 13:15:33 -0400

    data collection

--- api/handlers/driver.py
+++ api/handlers/driver.py
@@ -24,7 +24,8 @@ class DriverHandler(tornado.websocket.WebSocketHandler):
             min_speed = int(data['min_speed'])
             max_speed = int(data['max_speed'])
             if x and y:
-                await self.application.sensors_service.publish('Drive', {'x': x, 'y': y})
+                print(('Drive', {'x': x, 'y': y}))
+                self.application.sensors_service.publish('Drive', {'x': x, 'y': y})
                 left_speed, right_speed = self.application.driver.command_to_diff(x, y, min_speed, max_speed)
         except Exception as ex:
             print(ex)
