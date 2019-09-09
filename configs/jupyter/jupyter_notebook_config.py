74f7d7a42343952fd36a6ba2cc330a4d3cca9f7a
Mehrdad Pazooki <pazookime@gmail.com>
2019-09-09 17:30:17 -0400

    fix for jupyter iframe

--- configs/jupyter/jupyter_notebook_config.py
+++ configs/jupyter/jupyter_notebook_config.py
@@ -341,7 +341,7 @@ c.NotebookApp.terminals_enabled = True
 
 ## Supply overrides for the tornado.web.Application that the Jupyter notebook
 #  uses.
-c.NotebookApp.tornado_settings = { 'headers': { 'Content-Security-Policy': "frame-ancestors 'self' http://raspberrybot.local" } 
+c.NotebookApp.tornado_settings = { 'headers': { 'Content-Security-Policy': "frame-ancestors 'self' http://raspberrybot.local" }}
 
 ## Whether to trust or not X-Scheme/X-Forwarded-Proto and X-Real-Ip/X-Forwarded-
 #  For headerssent by the upstream reverse proxy. Necessary if the proxy handles
