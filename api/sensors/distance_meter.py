ccf6690a24bf425f514f30c8f7946b1dcba8eb8b
Mehrdad Pazooki <pazookime@gmail.com>
2019-09-10 22:37:13 -0400

    distance meter experiment!

--- api/sensors/distance_meter.py
+++ api/sensors/distance_meter.py
@@ -1,38 +1,44 @@
-#!/usr/bin/python
-import RPi.GPIO as GPIO
+import RPi.GPIO as io
 import time
 
+
+class USSensor():
+    '''Simple class for interfacing an HC-SR04'''
+
+    def __init__(self, echo=None, trigger=None):
+        self._echo = echo
+        self._trigger = trigger
+        self._pulse_begin = 0
+        self.distance = None  # default value for unmeasured distance
+        io.setmode(io.BCM)
+        io.setup(self._echo, io.IN)
+        io.setup(self._trigger, io.OUT)
+        io.add_event_detect(self._echo,
+                            io.BOTH,
+                            callback=self._stamp_times)
+        io.output(self._trigger, False)
+        time.sleep(0.000005)
+        io.output(self._trigger, True)
+        time.sleep(0.00001)
+        io.output(self._trigger, False)
+
+    def _stamp_times(self, channel):
+        if io.input(self._echo):
+            self._pulse_begin = time.time()
+        else:
+            self.distance = (time.time() - self._pulse_begin) * 34300 / 2
+            io.output(self._trigger, True)
+            time.sleep(0.00001)
+            io.output(self._trigger, False)
+
+
+# Test Driver
+sensor = USSensor(echo=17, trigger=23)
 try:
-      GPIO.setmode(GPIO.BOARD)
-
-      PIN_TRIGGER = 14
-      PIN_ECHO = 11
-
-      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-      GPIO.setup(PIN_ECHO, GPIO.IN)
-
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-      print "Waiting for sensor to settle"
-
-      time.sleep(2)
-
-      print "Calculating distance"
-
-      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-
-      time.sleep(0.00001)
-
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-      while GPIO.input(PIN_ECHO)==0:
-            pulse_start_time = time.time()
-      while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()
-
-      pulse_duration = pulse_end_time - pulse_start_time
-      distance = round(pulse_duration * 17150, 2)
-      print "Distance:",distance,"cm"
-
-finally:
-      GPIO.cleanup()
+    while True:
+        print(sensor.distance)
+        time.sleep(1)
+except KeyboardInterrupt:
+    print('\n' + '#' * 15 + '\tExiting\t' + '#' * 15)
+    io.cleanup()
+    print('GPIO Cleanup Complete!!')
