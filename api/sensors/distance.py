cb55fda070fd0f493d762271addf0ed563357cf5
Mehrdad Pazooki <pazookime@gmail.com>
2019-09-12 19:10:07 -0400

    distance meter

--- api/sensors/distance.py
+++ api/sensors/distance.py
@@ -1,65 +1,79 @@
-# Import required Python libraries
-import time
 import RPi.GPIO as GPIO
+import time
 
-# Use BCM GPIO references
-# instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+# A very simple Python interface to SFR05 module (probably will work with SFR04 too but I have no tried).
+# Just call measure() method and it will return you distance in centimeters or None.
+#
+# IMPORTANT NOTE: in order to read data from the sensor we need to measure width of an echo pulse
+# and we need to be accurate enough. This obviously is not going to work well if your CPU is heavily loaded.
+# Plus, Python with its GIL is just not the best environment for these things if you have other threads running.
+# But make no mistake - doing that in C is not a silver bullet either even if done properly with interrupts etc.
+# Sure, C version would have better accuracy but if you really want to avoid interference with other threads/processes,
+# you should use a sensor that times echo iself and then gives you result via digital interface (like I2C or SPI).
+#
+# The document I am referring throught the code is https://www.robot-electronics.co.uk/htm/srf05tech.htm
+#
+class SRF05:
+    def __init__(self, trigger_pin, echo_pin):
+        self.trigger_pin = trigger_pin
+        self.echo_pin = echo_pin
 
-# Define GPIO to use on Pi
-GPIO_TRIGGER = 23
-GPIO_ECHO    = 24
+        self.trigger_time = 0
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.echo_pin, GPIO.IN)
+        GPIO.setup(self.trigger_pin, GPIO.OUT)
 
-# Set pins as output and input
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)  # Trigger
-GPIO.setup(GPIO_ECHO, GPIO.IN)      # Echo
+    def measure(self):
+        now = self.time_us()
 
-def read():
-    # Set trigger to False (Low)
-    GPIO.output(GPIO_TRIGGER, False)
+        # "The SRF05 can be triggered as fast as every 50mS, or 20 times each second.
+        # You should wait 50ms before the next trigger, even if the SRF05 detects a close object and the echo pulse is shorter.
+        # This is to ensure the ultrasonic "beep" has faded away and will not cause a false echo on the next ranging."
+        pause = 50000 - (now - self.trigger_time)
+        if pause > 0:
+            self.sleep_us(pause)
 
-    print("Ultrasonic Measurement")
+        self.trigger()
 
-    # Allow module to settle
-    time.sleep(0.5)
+        self.trigger_time = self.time_us()
 
-    # Send 10us pulse to trigger
-    GPIO.output(GPIO_TRIGGER, True)
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+        # "The SRF05 will send out an 8 cycle burst of ultrasound at 40khz and raise its echo line high (or trigger line in mode 2)"
+        # Wait no longer than 30ms
+        if GPIO.wait_for_edge(self.echo_pin, GPIO.RISING, timeout=30) is None:
+            return None
 
-    # "The SRF05 will send out an 8 cycle burst of ultrasound at 40khz and raise its echo line high (or trigger line in mode 2)"
-    # Wait no longer than 30ms
-    if GPIO.wait_for_edge(GPIO_ECHO, GPIO.RISING, timeout=30) is None:
-        return None
+        start = self.time_us()
 
-    start = time.time()
+        # Measure pulse duration, again do not wait more than 30ms
+        # "If nothing is detected then the SRF05 will lower its echo line anyway after about 30mS."
+        if GPIO.wait_for_edge(self.echo_pin, GPIO.FALLING, timeout=30) is None:
+            return None
 
-    # Measure pulse duration, again do not wait more than 30ms
-    # "If nothing is detected then the SRF05 will lower its echo line anyway after about 30mS."
-    if GPIO.wait_for_edge(GPIO_ECHO, GPIO.FALLING, timeout=30) is None:
-        return None
+        end = self.time_us()
 
-    while GPIO.input(GPIO_ECHO)==1:
-        stop = time.time()
+        width = end - start
 
-    # Calculate pulse length
-    elapsed = stop-start
+        # ...and by that logic we should not have real measurement with pulse longer than 30ms anyway
+        if width > 30000:
+            return None
 
-    # Distance pulse travelled in that time is time
-    # multiplied by the speed of sound (cm/s)
-    distancet = elapsed * 34300
+        # "If the width of the pulse is measured in uS, then dividing by 58 will give you the distance in cm,
+        # or dividing by 148 will give the distance in inches. uS/58=cm or uS/148=inches."
+        return int(width / 58)
 
-    # That was the distance there and back so halve the value
-    distance = distancet / 2
+    def trigger(self):
+        # "You only need to supply a short 10uS pulse to the trigger input to start the ranging."
+        GPIO.output(self.trigger_pin, 1)
+        self.sleep_us(10)
+        GPIO.output(self.trigger_pin, 0)
 
-    print("Distance :", distance)
-    print("Elaspsed time :", elapsed)
-    print("Total distance :", distancet)
+    # Return time in microseconds
+    def time_us(self):
+        return int(time.time() * 1000000)
 
-    # Reset GPIO settings
-    GPIO.cleanup()
+    def sleep_us(self, us):
+        time.sleep(us / 1000000.0)
 
+sensor = SRF05(trigger_pin = 23, echo_pin = 24)
 while True:
-    read()
+    print(sensor.measure())
